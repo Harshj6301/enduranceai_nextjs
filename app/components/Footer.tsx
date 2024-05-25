50db339f6698999c12a6cbf9ead8bb3baa0811ae
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2024-05-25 17:05:57 +0530

    Add files via upload

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () =>{
+return (
+<footer className="bg-stone-950 text-white py-4 text-center justify-between p-1">
+        <p className='flex justify-between p-1'>&copy; 2024 Endurance. All rights reserved. <a href="mailto:harshvardhan@enduranceai.in" 
+        className="underline justify-between"><img src='assets\icons\email-2.png' className='w-5 h-5'></img></a></p>
+</footer>
+)
+};
+export default Footer;
